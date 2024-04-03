8f0b4e9bc314c1e4292f0545879c53abda7e8d1d
Rodro <rodro@Rodros-MacBook-Air.local>
2024-04-03 12:09:53 +0600

    first commit

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Banner from "../Banner/Banner";
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-6xl"> 
+                    <Banner></Banner>
+            </h1>
+        </div>
+    );
+};
+
+export default Home;
