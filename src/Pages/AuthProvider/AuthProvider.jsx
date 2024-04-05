9e05c39a536690a098ceff9cc8256b23664a9208
Rodro <rodro@Rodros-MacBook-Air.local>
2024-04-05 09:25:13 +0600

    services database added

--- src/Pages/AuthProvider/AuthProvider.jsx
+++ src/Pages/AuthProvider/AuthProvider.jsx
@@ -1,9 +1,43 @@
+/* eslint-disable react/prop-types */
+import { createContext, useEffect, useState } from "react";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import app from "../../firebase/firebase.config";
 
-const AuthProvider = () => {
+
+export const AuthContext = createContext();
+const auth = getAuth(app);
+
+
+const AuthProvider = ({children}) => {
+    const [user,setUser] = useState(null)
+    const [loading, setLoading] = useState(true);
+
+    const createUser = (email, password) =>{
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    useEffect(()=>{
+        const unsubscribe = onAuthStateChanged(auth, currentUser =>{
+            setUser(currentUser)
+            console.log('current user', currentUser)
+            setLoading(false);
+        });
+        return () =>{
+            return unsubscribe();
+        }
+    },[])
+
+    const authInfo = {
+        user,
+        loading,
+        createUser,
+    }
+
     return (
-        <div>
-            
-        </div>
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
     );
 };
 
--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,10 +1,28 @@
 import { Link } from 'react-router-dom';
 import img from '../../assets/login/login.svg';
+import { useContext } from 'react';
+import { AuthContext } from '../AuthProvider/AuthProvider';
 
 const SignUp = () => {
-    const loginSignUp = event =>{
+
+  const {createUser} = useContext(AuthContext)
+
+    const handleSignUp = event =>{
         event.preventDefault()
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(name,email,password)
+
+        createUser(email,password)
+        .then(result =>{
+          const user = result.user;
+          console.log(user)
+        })
+        .then(error => console.log(error));
     }
+    
     return (
         <div className="hero min-h-screen bg-base-200">
   <div className="hero-content flex-col lg:flex-row ">
@@ -13,7 +31,7 @@ const SignUp = () => {
     </div>
     <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
     <h1 className="text-4xl font-bold text-center mt-2">Sign Up!</h1>
-      <form onSubmit={loginSignUp} className="card-body">
+      <form onSubmit={handleSignUp} className="card-body">
       <div className="form-control">
           <label className="label">
             <span className="label-text">Name</span>
@@ -24,13 +42,13 @@ const SignUp = () => {
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="email" placeholder="email" className="input input-bordered" required />
+          <input type="email" placeholder="email" name='email' className="input input-bordered" required />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Create Password</span>
           </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
+          <input type="password" placeholder="password" name='password' className="input input-bordered" required />
           <label className="label">
             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
           </label>
